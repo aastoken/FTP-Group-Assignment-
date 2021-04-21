d79627d1b0550b32e25558b9e7177c385c6c1ffa
eduexile <eduexile@gmail.com>
2021-04-21 20:26:04 +0200

    Port command implementation

---
+++ FTP_Server/src/base/ServerInit.java
@@ -0,0 +1,13 @@
+package base;
+
+public class ServerInit {
+	
+	private static final ServerLogic logic = ServerLogic.getServer();
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//logic.getServer();
+
+	}
+
+}
