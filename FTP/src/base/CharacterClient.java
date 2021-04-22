00b45c788970693a6bd5a11ab3afc6e9a992d55f
Alberto Abad Sanz <alberto.abad.sanz@gmail.com>
2021-04-22 16:13:15 +0200

    client loop

--- FTP/src/base/CharacterClient.java
+++ FTP/src/base/CharacterClient.java
@@ -9,37 +9,41 @@ import java.io.*;
 public class CharacterClient {
 
 	public static void main(String args[]) {
-		int port = 1400;
+		int port = 1440;
 		Socket sCon = null;
 		BufferedReader input;
 		PrintWriter output;
 		
 		BufferedReader inputKeyboard;
-		String data;
+		String data = "";
 		String result;
 		
 		try {
-			// Connect to the server
-			sCon = new Socket("localhost", port);
+			while(data.compareTo("END") != 0)
+			{
+				// Connect to the server
+				sCon = new Socket("localhost", port);
+				
+				// Get the input/output from the socket
+				input = new BufferedReader(new InputStreamReader(sCon.getInputStream()));
+				output = new PrintWriter(sCon.getOutputStream(), true);
+				
+				// Get text from the keyboard
+				inputKeyboard = new BufferedReader(new InputStreamReader(System.in));
+				System.out.print("Write text (END to close the server): ");
+				data = inputKeyboard.readLine();
 			
-			// Get the input/output from the socket
-			input = new BufferedReader(new InputStreamReader(sCon.getInputStream()));
-			output = new PrintWriter(sCon.getOutputStream(), true);
 			
-			// Get text from the keyboard
-			inputKeyboard = new BufferedReader(new InputStreamReader(System.in));
-			System.out.print("Write text (END to close the server): ");
-			data = inputKeyboard.readLine();
+				// Send data to the server
+				output.println(data);
+				// Read data from the server
+				result = input.readLine();
+				
+				System.out.println("Data = " + data + " --- Result = " + result);		
 			
-			// Send data to the server
-			output.println(data);
-			// Read data from the server
-			result = input.readLine();
-			
-			System.out.println("Data = " + data + " --- Result = " + result);		
-			
-			// Close the connection
-			sCon.close();
+				// Close the connection
+				sCon.close();
+			}
 		}  catch(IOException e) {
 			System.out.println("Error: " + e);		
 	   }
