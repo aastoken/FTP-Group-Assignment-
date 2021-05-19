c1ac26eef3e4ac0ebeb30dfaa7a1f94134b066b3
pedro <pedro@LAPTOP-636HF8LS.home>
2021-05-19 14:49:09 +0200

    Descargar y subir archivos

--- FTP_Client_v2/src/main/Client.java
+++ FTP_Client_v2/src/main/Client.java
@@ -1,8 +1,13 @@
 package main;
 
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
@@ -22,6 +27,7 @@ public class Client {
 	
 	protected BufferedReader inputKeyboard = new BufferedReader(new InputStreamReader(System.in));
 	
+	private final String defaultPath = "C:\\Users\\pedro\\git\\FTP-Group-Assignment-\\FTP_Client_v2\\root_data\\";
 	
 	protected DataInputStream inputStream;
 	protected DataOutputStream outputStream;
@@ -82,8 +88,26 @@ public class Client {
 				System.out.println(input.readLine());
 				break;
 			case "RETR":
+				
+				// Elegir como llamar al archivo
+				// Se puede usar un substring del path
+				String fileName = words[1];
+				retrCommand(defaultPath + fileName);
+				System.out.println(input.readLine());
+				// Cerrar la conexión
+				dataSocket.close();
+								
 				break;
 			case "STOR":
+				
+				// Elegir como llamar al archivo
+				// Se puede usar un substring del path
+				String fileName2 = words[1];
+				storCommand(defaultPath + fileName2);
+				System.out.println(input.readLine());
+				// Cerrar la conexión
+				dataSocket.close(); 
+				
 				break;
 			case "QUIT":
 				System.out.println(input.readLine());
@@ -97,6 +121,66 @@ public class Client {
 			e.printStackTrace();
 		}
 		
+	}
+	
+	private void retrCommand(String path) {
+		// Gestionar los bytes que recibimos del server en el socket de datos
+		// Generar un archivo donde copiamos los bytes
+		try {
+			File downloadedFile = new File(path);
+			downloadedFile.createNewFile();
+			
+			BufferedInputStream originalBuffer = new BufferedInputStream(dataSocket.getInputStream());
+			
+			FileOutputStream  copy = new FileOutputStream (downloadedFile);
+			BufferedOutputStream copyBuffer = new BufferedOutputStream(copy);
+			
+			// Loop to read a file and write in another
+			byte [] array = new byte[1000];
+			int n_bytes = originalBuffer.read(array);
+			while (n_bytes > 0)
+			{
+				copyBuffer.write(array,0,n_bytes);
+				n_bytes=originalBuffer.read(array);
+			}
+			
+			originalBuffer.close();
+			copyBuffer.close();
+			
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void storCommand(String path) {
+		try {
+			
+			// 1 - Coger datos desde archivo
+			
+			FileInputStream original = new FileInputStream(path);
+			BufferedInputStream originalBuffer = new BufferedInputStream(original);
+			
+			// 2 - Mandarlo por el socket de datos
+						
+			BufferedOutputStream copyBuffer = new BufferedOutputStream(dataSocket.getOutputStream());
+						
+			// Loop to read a file and write in another
+			byte [] array = new byte[1000];
+			int n_bytes = originalBuffer.read(array);
+			while (n_bytes > 0)
+				{
+					copyBuffer.write(array,0,n_bytes);
+					n_bytes=originalBuffer.read(array);
+				}
+
+			// Close the files
+			originalBuffer.close();
+			copyBuffer.close();
+			
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public void closeConnection() {
